0ef89b7ea2e1855321c463ca30cbe0bcc1a076c0
LeoLyf07 <112721086+LeoLyf07@users.noreply.github.com>
2023-07-20 18:54:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+import turtle as t
+import time
+t.bgcolor("White")
+t.speed(2)
+t.title("Sumit")
+t.pensize(20)
+# t.color("Yellow","pink")
+t.begin_fill()
+t.color("Yellow","Brown")
+for i in range(16):
+    t.forward(100)
+    t.right(45)
+    t.left(34)
+    t.backward(120)
+for i in range(16):
+    t.forward(100)
+    t.right(45)
+    t.left(34)
+    t.backward(120)
+# for i in range(1):
+#     t.backward(50)
+#     t.left(45)
+#     t.backward(100)
+#     t.left(135)
+#     t.forward(100)
+#     t.left(45)
+#     t.forward(100)
+#     t.left(135)
+
+t.hideturtle()
+t.end_fill()
+# time.sleep(3)
+t.done()
+# import turtle
+
+# # Create a turtle object
+# my_turtle = turtle.Turtle()
+
+# # Set the background color
+# turtle.bgcolor("black")
+
+# # Set the turtle color and shape
+# my_turtle.color("red")
+# my_turtle.shape("turtle")
+
+# # Function to draw a flower petal
+# def draw_petal():
+#     my_turtle.forward(100)
+#     my_turtle.left(45)
+#     my_turtle.forward(100)
+#     my_turtle.left(135)
+#     my_turtle.forward(100)
+#     my_turtle.left(45)
+#     my_turtle.forward(100)
+#     my_turtle.left(135)
+
+# # Draw multiple petals to form a flower
+# for _ in range(8):
+#     draw_petal()
+#     my_turtle.left(45)
+
+# # Hide the turtle
+# my_turtle.hideturtle()
+
+# # Close the turtle graphics window
+# turtle.done()
