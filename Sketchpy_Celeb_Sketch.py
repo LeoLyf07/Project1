5f574413696f96cb46f39c27afc8352751fae083
LeoLyf07 <112721086+LeoLyf07@users.noreply.github.com>
2023-07-20 22:46:50 +0530

    Create Sketchpy_Celeb_Sketch.py

---
+++ Sketchpy_Celeb_Sketch.py
@@ -0,0 +1,3 @@
+from sketchpy import canvas # this line imports a canvas from sketchpy
+n = canvas.sketch_from_image("AnaDeArmaas.jpg") # this line needs a path of the image that you want to sketch
+n.draw() # finnaly this one draws the image 
