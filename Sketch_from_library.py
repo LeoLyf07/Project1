1e3f5c5ceceaf7dc2822f59ae13ec84574b631b5
LeoLyf07 <112721086+LeoLyf07@users.noreply.github.com>
2023-07-20 22:52:36 +0530

    Create Sketch_from_library.py

---
+++ Sketch_from_library.py
@@ -0,0 +1,11 @@
+from sketchpy import library # it is a library and it has so many images 
+abc = library.tom_holland() # from library we are calling a picture of Tom holland aka Spoidermon 🤣
+abc.draw() 
+
+
+# some library keys -
+
+ # rdj = robert downy juinior
+ # apj = abdul kalam 
+ # flag = Indian flag
+ # tom_holland = spiderman
